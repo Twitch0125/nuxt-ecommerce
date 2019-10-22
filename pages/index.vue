15dc345870eca076416274f71eedc9d61ce452c5
Kaleb Ercanbrack <hackerman@pop-os.localdomain>
2019-10-21 22:22:33 -0600

    cards

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,21 @@
   <v-row dense justify-center align-center>
     <v-col v-for="product in products" :key="product.id" cols="6" sm="4" md="2" lg="2">
       <v-skeleton-loader v-show="skeletonShown" type="card"></v-skeleton-loader>
-      <!-- <v-card></v-card> -->
+      <v-card :flat="flat" outlined hover :elevation="mobile ? 4:0">
+        <v-img
+          height="200px"
+          contain
+          @load="loading=false"
+          :src="product.img"
+          :alt="product.description"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular color="accent" indeterminate></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -13,7 +27,11 @@ import { mapMutations } from 'vuex'
 export default {
   data() {
     return {
-      skeletonShown: true
+      skeletonShown: false,
+      loading: true,
+      flat: true,
+      //checks if the viewport is below the xs threshold, i.e "<600px"
+      mobile: this.$vuetify.breakpoint.xsOnly
     }
   },
   mounted() {
