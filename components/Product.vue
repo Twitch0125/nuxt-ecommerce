6e21a6ed63f53890c4be045baa687c805905a3f9
Kaleb Ercanbrack <hackerman@pop-os.localdomain>
2019-10-22 19:43:39 -0600

    rating

--- components/Product.vue
+++ components/Product.vue
@@ -1,5 +1,6 @@
 <template>
   <v-card outlined hover :elevation="mobile ? 1:undefined">
+    <v-card-title class="card-title body-1 text-truncate">{{product.title}}</v-card-title>
     <v-img height="200px" contain :src="product.img" :alt="product.description">
       <template v-slot:placeholder>
         <v-row class="fill-height ma-0" align="center" justify="center">
@@ -7,13 +8,25 @@
         </v-row>
       </template>
     </v-img>
-    <v-card-title class="card-title body-1 text-truncate">{{product.title}}</v-card-title>
+    <v-card-subtitle>
+      <template style="display: inline">
+        <span>${{product.price}}</span>
+      </template>
+    </v-card-subtitle>
 
     <v-expand-transition>
       <div v-show="expand">
+        <v-rating
+          small
+          v-model="product.rating"
+          readonly
+          background-color="primary"
+          color="primary"
+        ></v-rating>
         <v-card-text>{{product.description}}</v-card-text>
       </div>
     </v-expand-transition>
+    <v-divider></v-divider>
     <v-card-actions>
       <v-btn text @click="expand = !expand">More...</v-btn>
     </v-card-actions>
